d7491c41892d44e966c0f8c7a82a26ab55637e61
naari3 <naari.named@gmail.com>
2022-09-13 06:11:25 +0900

    feat: build icon when running build scripts

--- build.rs
+++ build.rs
@@ -1,10 +1,11 @@
-// TODO: Use library which to make icon supported gradation
-// use std::path::Path;
+use std::path::Path;
 
 fn main() {
-    // println!("cargo:rerun-if-changed=icon/icon_plain.svg");
-    // let input = Path::new("./icon/icon_plain.svg");
-    // let output = Path::new("./icon/icon.ico");
+    println!("cargo:rerun-if-changed=icon/icon_plain.svg");
+    println!("cargo:rerun-if-changed=icon/icon.ico");
+    let input = Path::new("icon/icon_plain.svg");
+    let output = Path::new("icon/icon.ico");
 
-    // svg_to_ico::svg_to_ico(input, 256.0, output, &[16, 32, 48, 64, 96, 128, 256]).unwrap();
+    svg_to_ico::svg_to_ico(input, 96.0, output, &[32, 64, 256])
+        .expect("failed to convert svg to ico");
 }
